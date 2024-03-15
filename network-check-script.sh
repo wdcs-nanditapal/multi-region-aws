9cac08ba900ec381b16c27dadfc1924941086d3b
Nandita Pal <nandita.pal@codezeros.com>
2024-03-15 16:53:21 +0530

    mail added

--- network-check-script.sh
+++ network-check-script.sh
@@ -1,11 +1,14 @@
 #! /bin/bash
+ 
+echo "Enter Node1 IP:"
+read node1
+ping ${node1} -c 2 | grep 'packet loss' | awk -F',' '{print $3}' > output.txt
 
-ping 192.168.109.79 -c 2 | grep 'packet loss' | awk -F',' '{print $3}' > output.txt
-
-if [ cat output.txt == '+2 errors' ] 
+if [ "$(cat output.txt)" == '+2 errors' ]
 then
-	echo "Node1 is unreachable" | mail -s "Node monitoring alert" nandita.pal@codezeros.com
+	#echo "Node1 is unreachable" | mail -s "Node monitoring alert" nandita.pal@codezeros.com
+	echo "Node1 is unreachable"
 else
-        
+	echo "Node1 is reachable"
 fi
 
